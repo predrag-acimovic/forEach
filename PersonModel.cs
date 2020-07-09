9efa2b6f2e83668b73cbd1428e4266efcef32ba1
predrag-acimovic <predrag.acim74@gmail.com>
2020-07-09 11:28:45 +0200

    first commit

---
+++ PersonModel.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace myApp{
+
+    public class PersonModel
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> people = StringGetPeople();
+            foreach (string item in people)
+            {
+                Console.WriteLine(item);
+            }
+                Console.ReadLine();
+
+            List<PersonModel> personModels = ModelGetPerson();
+            foreach (PersonModel item in personModels)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName}");
+            }
+        }
+            private static List<PersonModel> ModelGetPerson(){
+            List<PersonModel> output = new List<PersonModel>();
+            output.Add(new PersonModel {FirstName = "Milan", LastName = "Milivojevic"});
+            output.Add(new PersonModel {FirstName = "Milan", LastName = "Josipovic"});
+            return output;
+            }
+        private static  List<string> StringGetPeople(){
+            List<string> output = new List<string>();
+            output.Add("Borivoje");
+            output.Add("Ivan");
+            output.Add("Gradimir");
+            output.Add("Milan");
+            output.Add("Milan");
+            output.Add("Predrag");
+            output.Add("Dusan");
+            return output;
+
+            }
+        
+    }
+}
